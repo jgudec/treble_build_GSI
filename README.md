04ffbd66c6637dc9cb739325ee8f57614ab238e4
jgudec <jakov.gudec@gmail.com>
2023-08-05 19:31:34 +0100

    Initial build-bot setup

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
+# DerpFest GSI
+
+## Build
+To get started with building Pixel Experience GSI, you'll need to get familiar with [Git and Repo](https://source.android.com/source/using-repo.html) as well as [How to build a GSI](https://github.com/phhusson/treble_experimentations/wiki/How-to-build-a-GSI%3F).
+- Create a new working directory for your Pixel Experience build and navigate to it:
+    ```
+    mkdir derpfest; cd derpfest
+    ```
+- Clone this repo:
+    ```
+    git clone https://github.com/jgudec/treble_build_gsi -b derpfest-13 treble_build_derp
+    ```
+- Finally, start the build script:
+    ```
+    bash treble_build_derp/build.sh
+    ```
+
+## Notes
+- If bluetooth calls or bluetooth media do not work well for you, make sure you have the "Use System Wide BT HAL" checkbox enabled on the Misc page of Treble App. If not, enable and reboot.
+- If you have a non-Samsung device with a Qualcomm chipset and VoLTE isn't working with the default IMS package provided by Treble App, try installing this [alternative IMS package](https://treble.phh.me/stable/ims-caf-s.apk).
+
 ## Credits
 These people have helped this project in some way or another, so they should be the ones who receive all the credit:
-- [DerpFest AOSP](https://github.com/DerpFest-AOSP/manifest)
-- [Phhusson](https://github.com/phhusson)
+- [Pixel Experience Team](https://download.pixelexperience.org/about)
+- [phhusson](https://github.com/phhusson)
 - [AndyYan](https://github.com/AndyCGYan)
-- [Ponces](https://github.com/ponces)
+- [eremitein](https://github.com/eremitein)
+- [kdrag0n](https://github.com/kdrag0n)
+- [Peter Cai](https://github.com/PeterCxy)
+- [haridhayal11](https://github.com/haridhayal11)
+- [sooti](https://github.com/sooti)
+- [Iceows](https://github.com/Iceows)
 - [ChonDoit](https://github.com/ChonDoit)
---
+++ build.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+echo
+echo "--------------------------------------"
+echo "         DerpFest 13.0 Buildbot       "
+echo "                  by                  "
+echo "                jgudec                "
+echo "--------------------------------------"
+echo
+
+set -e
+
+BL=$PWD/treble_build_derp
+BD=$HOME/builds
+
+
+export USE_CCACHE=1
+export CCACHE_COMPRESS=1
+export CCACHE_EXEC=/usr/bin/ccache
+export CCACHE_DIR=/mnt/ccache
+export CCACHE_EXEC=$(command -v ccache)
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+BUILD="DerpFest"
+DERPMK="$BL/derp.mk"
+
+initRepos() {
+    if [ ! -d .repo ]; then
+        echo "--> Initializing workspace"
+        repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13
+        echo
+
+        echo "--> Preparing local manifest"
+        mkdir -p .repo/local_manifests
+        cp $BL/manifest.xml .repo/local_manifests/derp.xml
+        echo
+    fi
+}
+
+syncRepos() {
+    echo "--> Syncing repos"
+    repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+    echo
+}
+
+applyPatches() {
+    echo "--> Applying prerequisite patches"
+    bash $BL/apply-patches.sh $BL prerequisite
+    echo
+
+    echo "--> Applying TrebleDroid patches"
+    bash $BL/apply-patches.sh $BL trebledroid
+    echo
+
+    echo "--> Applying personal patches"
+    bash $BL/apply-patches.sh $BL personal
+    echo
+
+    echo "--> Generating makefiles"
+    cd device/phh/treble
+    cp $BL/derp.mk .
+    bash generate.sh derp
+    cd ../../..
+    echo
+}
+
+setupEnv() {
+    echo "--> Setting up build environment"
+    source build/envsetup.sh &>/dev/null
+    mkdir -p $BD
+    echo
+}
+
+buildTrebleApp() {
+    echo "--> Building treble_app"
+    cd treble_app
+    bash build.sh release
+    cp TrebleApp.apk ../vendor/hardware_overlay/TrebleApp/app.apk
+    cd ..
+    echo
+}
+
+buildVariant() {
+    echo "--> Building treble_arm64_bvN"
+    lunch treble_arm64_bvN-userdebug
+    make -j$(nproc --all) installclean
+    make -j$(nproc --all) systemimage
+    mv $OUT/system.img $BD/system-treble_arm64_bvN.img
+    echo
+}
+
+generatePackages() {
+    echo "--> Generating packages"
+    xz -cv $BD/system-treble_arm64_bvN.img -T0 > $BD/DerpFest_arm64-ab-13.0-$BUILD_DATE-UNOFFICIAL.img.xz
+    rm -rf $BD/system-*.img
+    echo
+}
+
+START=`date +%s`
+BUILD_DATE="$(date +%Y%m%d)"
+
+if [ $1 != "nosync" ]
+then
+echo "ATTENTION: syncing/patching skipped!"
+initRepos
+syncRepos
+applyPatches
+fi
+setupEnv
+buildTrebleApp
+buildVariant
+generatePackages
+
+
+END=`date +%s`
+ELAPSEDM=$(($(($END-$START))/60))
+ELAPSEDS=$(($(($END-$START))-$ELAPSEDM*60))
+
+echo "--> Buildbot completed in $ELAPSEDM minutes and $ELAPSEDS seconds"
+echo
